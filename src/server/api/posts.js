c2b021b73582e534f10a0c73a846d86bdbbe1d14
Alexandru Chirițescu <alex@alexchiri.com>
2016-01-02 11:53:04 +0100

    Naive approach to serving the posts content

--- src/server/api/posts.js
+++ src/server/api/posts.js
@@ -1,8 +1,31 @@
 import koaRouter from 'koa-router';
+import fs from 'fs';
+import path from 'path'
+import readline from 'readline';
+
 const router = koaRouter();
 
-router.get('/posts', function*() {
-    this.body="many many posts";
+router.get('/posts', function*(next) {
+    const postsBaseDir = path.resolve(__dirname, '../../../posts');
+    const posts = fs.readdirSync(postsBaseDir);
+
+    var postData = {};
+    for (let i = 0, len = posts.length; i < len; i++) {
+        let postFilename = posts[i];
+        const postFilePath = path.resolve(postsBaseDir, postFilename);
+
+        var lineReader = readline.createInterface({
+            input: fs.createReadStream(postFilePath)
+        });
+
+        postData[postFilename] = [];
+
+        lineReader.on('line', function (line) {
+            postData[postFilename].push(line);
+        });
+    }
+
+    this.body = postData;
 });
 
 export default router.middleware();
