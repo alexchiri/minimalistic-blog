5884798a3f122edf001a271a13d8b3ec62aa61d2
Alexandru Chirițescu <alex@alexchiri.com>
2016-01-02 16:18:05 +0100

    Read all the lines of the md files and return them in the response

--- src/server/api/posts.js
+++ src/server/api/posts.js
@@ -5,10 +5,20 @@ import readline from 'readline';
 
 const router = koaRouter();
 
-function lineReaderThunk(lineReader) {
+function readPostFile(postFilePath) {
     return function(done) {
-        lineReader.on('line', function (line) {
-            done(null, line);
+        var lines = [];
+
+        var lineReader = readline.createInterface({
+            input: fs.createReadStream(postFilePath)
+        });
+
+        lineReader.on('line', function(line){
+            lines.push(line);
+        });
+
+        lineReader.on('close', function(){
+            done(null, lines);
         });
     }
 }
@@ -16,20 +26,13 @@ function lineReaderThunk(lineReader) {
 router.get('/posts', function*(next) {
     const postsBaseDir = path.resolve(__dirname, '../../../posts');
     const posts = fs.readdirSync(postsBaseDir);
-
     var postData = {};
+
     for (let i = 0, len = posts.length; i < len; i++) {
         let postFilename = posts[i];
         const postFilePath = path.resolve(postsBaseDir, postFilename);
 
-        var lineReader = readline.createInterface({
-            input: fs.createReadStream(postFilePath)
-        });
-
-        postData[postFilename] = [];
-
-        var line = yield lineReaderThunk(lineReader);
-        postData[postFilename].push(line);
+        postData[postFilename] = yield readPostFile(postFilePath);
     }
 
     this.body = postData;
